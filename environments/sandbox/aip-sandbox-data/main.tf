d50b141d66661254f162934a82fe0f87af8b8f40
Soravis Prommas <soravis.pr@bluebik.com>
2025-11-19 20:33:43 +0700

    updated on aip-sandbox-data

--- environments/sandbox/aip-sandbox-data/main.tf
+++ environments/sandbox/aip-sandbox-data/main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "~> 5.0"
-    }
-
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "~> 2.23"
-    }
-  }
-}
-
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
 module "network" {
   source              = "../../../modules/network"
   project_id          = var.project_id
