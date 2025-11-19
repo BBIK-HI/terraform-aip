720c656c990092764c52deaa72c77728eb943b18
Soravis Prommas <soravis.pr@bluebik.com>
2025-11-19 11:23:37 +0700

    test

--- environments/sandbox/aip-sandbox-application/main.tf
+++ environments/sandbox/aip-sandbox-application/main.tf
@@ -17,11 +17,11 @@ provider "google" {
   region  = var.region
 }
 
-# module "project_network" {
-#   source              = "modules\network"
-#   project_id          = var.project_id
-#   network_name        = "vpc-sandbox"
-#   subnet_name         = "snet-prd-gke-portal"
-#   subnet_cidr_range   = "10.250.0.0/24"
-#   region              = var.region
-# }
+module "project_network" {
+  source              = "modules\network"
+  project_id          = var.project_id
+  network_name        = "vpc-sandbox"
+  subnet_name         = "snet-prd-gke-portal"
+  subnet_cidr_range   = "10.250.0.0/24"
+  region              = var.region
+}
